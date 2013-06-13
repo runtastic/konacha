c79e6ec90902803be007e30acea3ea7703ae74f2
Michael Klein <michael@firstiwaslike.com>
2013-06-13 17:21:18 +0200

    Add make konacha work with paper_trail gem.
    We have to override paper_trail's user_for_paper_trail method
    to make konacha work with paper_trail.

--- app/controllers/konacha/specs_controller.rb
+++ app/controllers/konacha/specs_controller.rb
@@ -13,5 +13,10 @@ module Konacha
       @spec = Konacha::Spec.find_by_name(params[:name])
       @stylesheets = Konacha::Engine.config.konacha.stylesheets
     end
+
+    # Make konacha work with papertrail.
+    def user_for_paper_trail
+      nil
+    end
   end
 end
